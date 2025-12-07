1d48b044b64b2ee44c1d0ffc7c61dd658c33aee4
Som Sinha <som@bern.somsinha.local>
2025-12-07 10:32:56 +0000

    new: make cli

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
+use crate::daily::day1::day1;
 use crate::daily::day2::day2;
+use clap::Parser;
 
 mod daily;
 
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg[short, long]]
+    day: u8,
+}
+
 fn main() {
-    let x =day2();
+    let args = Args::parse();
+    match args.day {
+        1 => day1(),
+        2 => day2(),
+        _ => unimplemented!("Other days have not yet been implemented"),
+    }
 }
